fef5c09d110e58e8aa275fdf08e33b34bde0cd8b
chirartem <chirartem@gmail.com>
2022-06-27 10:28:39 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// let button = document.querySelectorAll('.products__delete-btn');
+// let passwordDelete;
+// console.log(button);
+
+// button.forEach((item, index, array) => {
+//   item.onclick = function() {
+// 		passwordDelete = prompt('Введите пароль');
+// 		console.log(passwordDelete);
+// 	};
+// });
